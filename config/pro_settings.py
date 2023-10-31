c4b2caa49ca93869622b24fd8944a214c69edd31
oyatulloh <102955545+oyatullohm@users.noreply.github.com>
2023-10-31 15:32:15 +0500

    Update pro_settings.py

--- config/pro_settings.py
+++ config/pro_settings.py
@@ -5,12 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = ['*']
 
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.parse('postgres://oyatilla:MZpaOJbZGTPjlAmA57jPifVnZ2n4eSnN@dpg-cl0c79i37rbc739666qg-a.oregon-postgres.render.com/oyatilla')
+
 
 __all__ = ['ALLOWED_HOSTS',"DATABASES"]
